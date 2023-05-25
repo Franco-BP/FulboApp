cc8885dba18b9d56e9848530580c111a95a0dc77
chancha-santafe <serranoavila@gmail.com>
2023-05-24 23:02:13 -0300

    Cambios en las Entidaddes y DTO, agregado de Serializable

--- src/main/java/fulboapp/dto/UserDTO.java
+++ src/main/java/fulboapp/dto/UserDTO.java
@@ -1,9 +1,12 @@
 package fulboapp.dto;
 
+import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class UserDTO {
+public class UserDTO implements Serializable{
+
+  private static final long serialVersionUID = 6455781916662830542L;
 
   private Long id;
 
--- src/main/java/fulboapp/model/User.java
+++ src/main/java/fulboapp/model/User.java
@@ -1,9 +1,15 @@
 package fulboapp.model;
 
+import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class User {
+public class User implements Serializable{
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 3719638096558536431L;
 
   private Long id;
 
